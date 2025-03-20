4b0b72a68d8546f77743eb416f684ead85fe726c
Seth Lawler <slawler@dewberry.com>
2025-03-20 12:40:41 -0400

    add draft structure and hec-ras schema

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ffrd-conventions
 Naming conventions and standards for FFRD pilots
+
+Draft conventions under development:
+
+[HEC-RAS](hec-ras/readme.md)
---
+++ to_markdown.py
@@ -0,0 +1,34 @@
+"""Utility to export schemas for viewing in markdown table."""
+
+import json
+
+import pandas as pd
+
+
+def props_to_header(schema_props: dict) -> str:
+    """Export schema top level props to doc."""
+    return f"""# HEC-RAS Naming Conventions
+    Draft {schema_props['title']}\n
+    schema version: {schema_props['version']}\n
+    Description: {schema_props['title']}\n
+    """
+
+
+with open("hec-ras/json-schema/schema.json", "r") as f:
+    schema = json.load(f)
+    properties_data = [
+        {
+            "Property": prop_name,
+            "Type": prop_details.get("type", ""),
+            "Description": prop_details.get("description", ""),
+            "Pattern": prop_details.get("pattern", ""),
+            "Examples": ", ".join(prop_details.get("examples", [])),
+        }
+        for prop_name, prop_details in schema["properties"].items()
+    ]
+    df = pd.DataFrame(properties_data)
+
+
+with open("hec-ras/readme.md", "w") as f:
+    f.write(props_to_header(schema))
+    f.write("\n" + df.to_markdown(index=False) + "\n")
